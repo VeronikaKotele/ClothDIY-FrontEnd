c5354c0d5e61943affcf0056d0d4d85f1fcebc57
ver <magictimejkrowling@gmail.com>
2026-04-24 07:01:51 +0200

    basic preview scene

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,23 @@
-import "@babylonjs/core/Debug/debugLayer.js";
-import "@babylonjs/loaders/OBJ/index.js";
+import { registerBuiltInLoaders } from "@babylonjs/loaders/dynamic.js";
 import {
   Engine,
   Scene,
   ArcRotateCamera,
   Vector3,
   HemisphericLight,
-  MeshBuilder,
+  Tools,
+  AppendSceneAsync,
+  ImportMeshAsync,
+  FreeCameraGamepadInput,
 } from "@babylonjs/core";
-import { AppendSceneAsync } from "@babylonjs/core/Loading/sceneLoader.js";
+
+import * as BABYLON from "@babylonjs/core";
+
+registerBuiltInLoaders();
 
 class App {
   private canvas: HTMLCanvasElement;
   private engine: Engine;
-  private inspectorLoaded = false;
 
   constructor() {
     // create the canvas html element and attach it to the webpage
@@ -33,44 +37,27 @@ class App {
       "Camera",
       Math.PI / 2,
       Math.PI / 2,
-      2,
+      20,
       Vector3.Zero(),
       scene,
     );
     camera.attachControl(this.canvas, true);
 
     new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
-    MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
 
-    // Load an OBJ model served by webpack dev server from /3dModels.
-    await AppendSceneAsync("/3dModels/body.obj", scene);
+    //Load an OBJ model served by webpack dev server from /3dModels.
+    var bodyMeshes = await ImportMeshAsync("/3dModels/body.obj", scene);
+    bodyMeshes.meshes.forEach((mesh) => {
+      console.log("TransformNode:", mesh.name, mesh.position);
+      mesh.translate(new Vector3(0, -10, 0), BABYLON.Space.WORLD);
+      mesh.freezeWorldMatrix();
+    });
 
     return scene;
   }
 
   async init(): Promise<void> {
     const scene = await this.createScene();
-
-    // hide/show the Inspector
-    window.addEventListener("keydown", async (ev) => {
-      // Shift+Ctrl+Alt+I
-      if (
-        ev.shiftKey &&
-        ev.ctrlKey &&
-        ev.altKey &&
-        (ev.key === "I" || ev.key === "i")
-      ) {
-        if (scene.debugLayer.isVisible()) {
-          scene.debugLayer.hide();
-        } else {
-          if (!this.inspectorLoaded) {
-            await import("@babylonjs/inspector");
-            this.inspectorLoaded = true;
-          }
-          scene.debugLayer.show();
-        }
-      }
-    });
 
     window.addEventListener("resize", () => {
       this.engine.resize();
